661ca774de64f1a2fe9ee21bcd6d8a6cebc7b2cc
misawayb <misawa.y.b@gmail.com>
2026-04-17 18:57:57 +0900

    購入・住所変更・Stripe関連のルートを追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\AddressController;
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
@@ -17,6 +19,12 @@ Route::middleware('auth')->group(function(){
     Route::patch('/mypage/profile', [UserController::class, 'update'])->name('user.update');
     Route::post('/item/{item_id}/like', [LikeController::class, 'store'])->name('like.store');
     Route::post('/item/{item_id}/comment',[CommentController::class,'store'])->name('comment.store');
+    Route::get('/purchase/address/{item_id}', [AddressController::class, 'show'])->name('address.show');
+    Route::post('/purchase/address/{item_id}', [AddressController::class, 'store'])->name('address.store');
+    Route::get('/purchase/{item_id}',[PurchaseController::class,'show'])->name('purchase.show');
+    Route::post('/purchase/{item_id}', [PurchaseController::class, 'store'])->name('purchase.store');
+    Route::get('/purchase/success/{item_id}', [PurchaseController::class, 'success'])->name('purchase.success');
+    Route::get('/purchase/cancel/{item_id}', [PurchaseController::class, 'cancel'])->name('purchase.cancel');
 });
 
 Route::get('/', [ItemController::class, 'index'])->name('item.index');
